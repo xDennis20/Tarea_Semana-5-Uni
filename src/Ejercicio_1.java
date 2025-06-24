a578b35be58f9a46082327783cb6b45c4f64d57d
xDennis20 <dennis_6@outlook.com>
2025-06-24 09:54:13 -0500

    Primer Ejercicio hecho

---
+++ src/Ejercicio_1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        List<String> asignaturas = new ArrayList<>();
+        asignaturas.add("Matematicas");
+        asignaturas.add("Programacion II");
+        asignaturas.add("Desarrollo web");
+        asignaturas.add("Seguridad Informatica");
+        System.out.println("Lista de Asignaturas: "+ asignaturas);
+    }
+}
