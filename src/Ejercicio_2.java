f5273b826cca4b5d9f16e249481c57e613ad7ceb
xDennis20 <dennis_6@outlook.com>
2025-06-24 10:04:15 -0500

    Segundo Ejercicio

--- src/Ejercicio_2.java
+++ src/Ejercicio_2.java
@@ -1,3 +1,15 @@
-public class Ejercicio_2 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        List<String> asignaturas = new ArrayList<>();
+        asignaturas.add("Matematicas");
+        asignaturas.add("Programacion II");
+        asignaturas.add("Desarrollo web");
+        asignaturas.add("Seguridad Informatica");
+        for(String asginatura: asignaturas){ //For each
+            System.out.println("Yo estudio: "+asginatura);
+        }
+    }
 }
